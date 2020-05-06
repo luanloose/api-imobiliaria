0e63b2b062ea2d5c6fa6a064d5ba620324c7e156
Luan <luan.gtecnologia@gmail.com>
2020-05-06 14:29:38 -0300

    Feito ate a autenticação JWT

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,39 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->namespace('Api')->group(function(){
+
+	Route::post('login', 'Auth\\LoginJwtController@login')->name('login');
+	Route::get('logout', 'Auth\\LoginJwtController@logout')->name('logout');
+	Route::get('refresh', 'Auth\\LoginJwtController@refresh')->name('refresh');
+
+	Route::get('/search', 'RealStateSearchController@index')->name('search');
+	Route::get('/search/{real_state_id}', 'RealStateSearchController@show')->name('search_single');
+
+	Route::group(['middleware' => ['jwt.auth']], function(){
+
+		Route::name('real_states.')->group(function(){
+
+			Route::resource('real-states', 'RealStateController');
+
+		});
+
+		Route::name('users.')->group(function(){
+
+			Route::resource('users', 'UserController');
+		});
+
+		Route::name('categories.')->group(function(){
+			Route::get('categories/{id}/real-states', 'CategoryController@realState');
+
+			Route::resource('categories', 'CategoryController');
+		});
+
+		Route::name('photos.')->prefix('photos')->group(function(){
+			Route::delete('/{id}', 'RealStatePhotoController@remove')->name('delete');
+
+			Route::put('/set-thumb/{photoId}/{realStateId}', 'RealStatePhotoController@setThumb')->name('delete');
+		});
+	});
+});
